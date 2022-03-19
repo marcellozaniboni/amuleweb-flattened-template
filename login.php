0765a55f14a9398ffdabb965c9b8df8b097e01b6
Marcello Zaniboni <93586459+marcellozaniboni@users.noreply.github.com>
2022-03-19 13:03:04 +0100

    First commit

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>aMule control panel</title>
+	<script language="JavaScript" type="text/javascript">
+	function login_init() {
+		document.login.pass.focus();
+	}
+	</script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body onload="login_init();" style="font-family: Arial, Helvetica, sans-serif; font-size: 16px" background="images/fond.gif" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" valign="middle">
+	<center>
+	&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>
+	<font color="#003161"><big><b>aMuleWeb</b><br/>authentication</big></font><br/>&nbsp;<br/>
+	<form action="" method="post" name="login">
+		Enter password:
+		<input name="pass" size="20" value="" type="password">
+		&nbsp;
+		<input name="submit" type="submit" value="Submit">
+	</form>
+	</center>
+</body>
+</html>
